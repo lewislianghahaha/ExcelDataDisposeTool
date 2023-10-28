e76d4095f375c71cd05a440c5021573091263be9
Lewis <heshanpin2016@163.com>
2023-10-28 15:08:37 +0800

    添加项目文件。

---
+++ ExcelDataDisposeTool/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ExcelDataDisposeTool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tmclose = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmimport = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtadd = new System.Windows.Forms.TextBox();
+            this.btnsetadd = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tmclose,
+            this.tmimport});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(386, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tmclose
+            // 
+            this.tmclose.Name = "tmclose";
+            this.tmclose.Size = new System.Drawing.Size(44, 21);
+            this.tmclose.Text = "关闭";
+            // 
+            // tmimport
+            // 
+            this.tmimport.Name = "tmimport";
+            this.tmimport.Size = new System.Drawing.Size(73, 21);
+            this.tmimport.Text = "导入Excel";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnsetadd);
+            this.panel1.Controls.Add(this.txtadd);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 33);
+            this.panel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "设置导出地址:";
+            // 
+            // txtadd
+            // 
+            this.txtadd.Enabled = false;
+            this.txtadd.Location = new System.Drawing.Point(87, 4);
+            this.txtadd.Name = "txtadd";
+            this.txtadd.Size = new System.Drawing.Size(211, 21);
+            this.txtadd.TabIndex = 5;
+            // 
+            // btnsetadd
+            // 
+            this.btnsetadd.Location = new System.Drawing.Point(304, 3);
+            this.btnsetadd.Name = "btnsetadd";
+            this.btnsetadd.Size = new System.Drawing.Size(76, 24);
+            this.btnsetadd.TabIndex = 6;
+            this.btnsetadd.Text = "浏览";
+            this.btnsetadd.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 61);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "Excel数据处理工具";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tmclose;
+        private System.Windows.Forms.ToolStripMenuItem tmimport;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnsetadd;
+        private System.Windows.Forms.TextBox txtadd;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
